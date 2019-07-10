437d37180177e5074fdf196b2f6e578acd7eca3d
wq <guddqs@icloud.com>
2019-07-10 23:19:42 +0800

    del

--- base/deploy-mountain.sh
+++
@@ -1,8 +0,0 @@
-cd superpom
-mvn deploy -P mountain,mountain-nexus,!local,!local-nexus
-
-cd ../platform
-mvn deploy -P mountain,mountain-nexus,!local,!local-nexus
-
-cd ../system
-mvn deploy -P mountain,mountain-nexus,!local,!local-nexus
